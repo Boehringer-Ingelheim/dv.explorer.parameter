d400342697181d6188faf7382fb48b5a871c0e64
zsigmas <6012476+zsigmas@users.noreply.github.com>
2024-08-28 07:55:29 +0000

    Deploying to gh-pages from @ Boehringer-Ingelheim/dv.explorer.parameter@87624affbf5e39f008f40b8fbcbfe67f341e741f 🚀

--- R/utils-nongxp.R
+++
@@ -1,7 +0,0 @@
-non_gxp_tag <- function(id) {
-  shiny::div(
-    shiny::tags[["p"]]("\u26A0 This is a NonGxP module", style = "display: inline-block"),
-    class = "alert alert-warning dv-nongxp-info-box",
-    id = id
-  )
-}
--- README.md
+++
@@ -1,9 +0,0 @@
-# dv.explorer.parameter
-
-# Installation
-
-``` r
-if (!require("remotes")) install.packages("remotes")
-remotes::install_github("Boehringer-Ingelheim/dv.explorer.parameter")
-```
-
