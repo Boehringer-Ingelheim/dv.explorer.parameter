388f4b52d48fb043d09006fd707719c83c4b20ed
Luis Moris Fernandez <luis.moris.fernandez@gmail.com>
2025-01-16 11:39:47 +0100

    deprecate mod_boxplot_papo

--- R/mod_boxplot.R
+++ R/mod_boxplot.R
@@ -708,6 +708,9 @@ boxplot_server <- function(id,
 }
 
 #' Invoke boxplot module
+#' 
+#' @details `mod_boxplot_papo` is a deprecated function that was required to use the jumping feature in combination
+#' with `dv.papo` but is no longer required. The function is still available for compatibility reasons.
 #'
 #' @param module_id `[character(1)]`
 #'
@@ -719,8 +722,7 @@ boxplot_server <- function(id,
 #'
 #' @param server_wrapper_func `[function()]`
 #'
-#' A function that will be applied to the server returned value.
-#' Only for advanced use. See the example in mod_box_plot_papo
+#' A function that will be applied to the server returned value. Its default value will work for the current cases.
 #'
 #' @param receiver_id `[character(1)]`
 #'
@@ -749,7 +751,7 @@ mod_boxplot <- function(module_id,
                         default_main_group = NULL,
                         default_sub_group = NULL,
                         default_page_group = NULL,
-                        server_wrapper_func = identity) {
+                        server_wrapper_func = function(x) list(subj_id = x)) {
   mod <- list(
     ui = boxplot_UI,
     server = function(afmm) {
@@ -863,10 +865,9 @@ mod_boxplot <- CM$module(mod_boxplot, check_mod_boxplot, dataset_info_boxplot)
 
 #' @describeIn mod_boxplot Boxplot wrapper when its output is fed into papo module
 #' @export
-mod_boxplot_papo <- function(...) {
-  args <- list(...)
-  args[["server_wrapper_func"]] <- function(x) list(subj_id = x)
-  do.call(mod_boxplot, args)
+mod_boxplot_papo <- function(...) {  
+  .Deprecated("mod_boxplot_papo", msg = "'mod_boxplot_papo' is no longer required and should be replaced by 'mod_boxplot'. It is still available for compatibility purposes") # nolint
+  mod_boxplot(...)  
 }
 
 # Data manipulation
