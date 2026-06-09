89d5f5bab27bae994e39bd27d9a88060fd0670a9
Methodios Typou <methodios.typou.ext@boehringer-ingelheim.com>
2026-06-09 06:47:17 +0000

    Resolve missmatch with test

--- R/mod_boxplot.R
+++ R/mod_boxplot.R
@@ -1024,11 +1024,11 @@ check_mod_boxplot <- function(
     cat_var, par_var, value_vars, x_axis_vars, visit_var, anlfl_vars, subjid_var, quantile_type,
     default_cat, default_par, default_x_axis_var, default_x_axis_vals, default_visit, default_value,
     default_main_group, default_sub_group, default_page_group, server_wrapper_func) {
+
   err <- CM$container()
-  warn <- CM$container()
 
   # TODO: Replace this function with a generic one that performs the checks based on mod_boxplot_API_spec.
-  # Something along the lines of OK <- CM$check_API(mod_corr_hm_API_spec, args = match.call(), warn, err)
+  # Something along the lines of OK <- CM$check_API(mod_corr_hm_API_spec, args = match.call(), err)
   OK <- check_mod_boxplot_auto(
     afmm, datasets, module_id, bm_dataset_name, group_dataset_name, receiver_id,
     cat_var, par_var, value_vars, x_axis_vars, visit_var, anlfl_vars, subjid_var, quantile_type,
@@ -1046,29 +1046,15 @@ check_mod_boxplot <- function(
   )
 
   #ahwopu
-  # if (OK[["subjid_var"]] && OK[["cat_var"]] && OK[["par_var"]] && OK[["x_axis_vars"]] && OK[["anlfl_vars"]]) {
-  #
-  #   if (!is.null(anlfl_vars)) {
-  #     # Check grouping values are unique for specified analysis flags
-  #     for (anlfl_var in anlfl_vars) {
-  #       CM$check_unique_sub_cat_par_vis(
-  #         datasets, "bm_dataset_name", bm_dataset_name,
-  #         subjid_var, cat_var, par_var, x_axis_vars, anlfl_var,
-  #         warn = warn, err = err
-  #       )
-  #     }
-  #   } else {
-  #     # Check grouping values are unique without subsetting on analysis flags
-  #     CM$check_unique_sub_cat_par_vis(
-  #       datasets, "bm_dataset_name", bm_dataset_name,
-  #       subjid_var, cat_var, par_var, x_axis_vars,
-  #       warn = warn, err = err
-  #     )
-  #   }
-  #
-  # }
+  if (OK[["subjid_var"]] && OK[["cat_var"]] && OK[["par_var"]] && OK[["x_axis_vars"]] && OK[["anlfl_vars"]]) {
+    check_unique_sub_cat_par_vis(
+      datasets, "bm_dataset_name", bm_dataset_name,
+      subjid_var, cat_var, par_var, visit_var, anlfl_vars,
+      err = err
+    )
+  }
 
-  res <- list(warnings = warn[["messages"]], errors = err[["messages"]])
+  res <- list(errors = err[["messages"]])
   return(res)
 }
 
@@ -1078,7 +1064,7 @@ dataset_info_boxplot <- function(bm_dataset_name, group_dataset_name, ...) {
   return(list(all = unique(c(bm_dataset_name, group_dataset_name)), subject_level = group_dataset_name))
 }
 
-mod_boxplot <- CM$module(mod_boxplot, check_mod_boxplot, dataset_info_boxplot) #, map_afmm_mod_boxplot_auto)
+mod_boxplot <- CM$module(mod_boxplot, check_mod_boxplot, dataset_info_boxplot)
 
 
 # Data manipulation
