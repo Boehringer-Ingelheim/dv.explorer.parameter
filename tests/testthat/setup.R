956a1df985fe874adec18c44793e5485c3240c89
zxBIB Lechón,Miguel (MED BDS) EXTERNAL <miguel.lechon.ext@boehringer-ingelheim.com>
2026-08-25 18:24:32 +0200

    Add history to "test communication with papo" snippet.

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -90,7 +90,7 @@ expect_r2d3_svg <- function(app, query_list) {
   })
 }
 
-# YT#VH3b377124b258c8f7c5c9951207153ecb#VH0bf15c0db690dfd3fac713f3c9b61f66#
+# YT#VHcb39336d4fcc5b936a31ce2666bcf27c#VH3b377124b258c8f7c5c9951207153ecb#
 
 #' Test harness for communication with `dv.papo`.
 #'
@@ -99,6 +99,8 @@ expect_r2d3_svg <- function(app, query_list) {
 #' @param trigger_input_id Fully namespaced input ID that, when set to a subject ID value,
 #'                         should make the module send `dv.papo` a message.
 test_communication_with_papo <- function(mod, data, trigger_input_id, papo_spec_id, papo_spec_text) {
+  # 2026-08-25: [fix] Support new dv.manager >= 3.1.0 afmm field names
+
   datasets <- shiny::reactive(data)
 
   afmm <- list(
@@ -147,7 +149,7 @@ test_communication_with_papo <- function(mod, data, trigger_input_id, papo_spec_
 
     trigger_subject_selection <- function(subject_id) {
       set_input_params <- append(
-        as.list(setNames(subject_id, trigger_input_id)),
+        as.list(stats::setNames(subject_id, trigger_input_id)),
         list(allow_no_input_binding_ = TRUE, priority_ = "event")
       )
       do.call(app$set_inputs, set_input_params)
